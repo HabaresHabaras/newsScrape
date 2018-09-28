174960a12c2dac8cafd80de433b70c1f3a8600c2
Dali Gala <habaresbit@gmail.com>
2018-09-28 02:16:32 -0400

    Make it better

--- public/app.js
+++ public/app.js
@@ -15,41 +15,7 @@ $.getJSON("/articles", function (data) {
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "button", function () {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function (data) {
-      console.log(data);
-      // The titla3wv e of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
-
-// Whenever someone clicks a p tag
-$(document).on("click", "button", function () {
+$(document).on("click", ".noteButton", function () {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
@@ -114,16 +80,14 @@ $(document).on("click", "#savenote", function () {
 $(document).on("click", "#saveButton", function () {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
+  // Using a patch method to be semantic since this is an update to an existing record in our collection
   $.ajax({
-    method: "POST",
+    method: "PUT",
     url: "/articles/" + thisId,
-    data: 
-    JSON.stringify({
-      saved : true
-    })
-  })
+    data: {
+      saved: true
+    }
+  });
 });
 //TO DO
 //MONGO
